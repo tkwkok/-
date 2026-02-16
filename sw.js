2bb85ad446df11d736140632c7822295c8132ac3
tkwkok <76809253+tkwkok@users.noreply.github.com>
2026-02-16 13:24:03 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'myeonggyeong-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/app.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
